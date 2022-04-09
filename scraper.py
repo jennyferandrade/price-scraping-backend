87979fc0c0ff55e64fb2bc02758a841b59ed0b3c
jennyferandrade <jennyferandradev@gmail.com>
2022-04-09 08:52:18 +0200

    read pages from amazon

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
 HEADERS = (
@@ -11,4 +12,26 @@ HEADERS = (
 price_scraping_source = pd.read_csv('products.csv', sep=';')
 prod_tracker_URLS = price_scraping_source.url
 page = requests.get(prod_tracker_URLS[0], headers=HEADERS)
-print(page)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+title = soup.find(id='productTitle').get_text().strip()
+try:
+    # you will get the price like this "2.804,15€", so we are transforming to get something like this "2804.15"
+    price = float(
+        soup.select_one('.a-offscreen').get_text().replace('.', '').replace('€', '').replace(',', '.').strip()
+    )
+except:
+    price = ''
+
+
+try:
+    soup.select('#availability .a-color-state')[0].get_text().strip()
+    stock = 'Out of Stock'
+except:
+    try:
+        soup.select('#availability .a-color-price')[0].get_text().strip()
+        stock = 'Out of Stock'
+    except:
+        stock = 'Available'
+
+print(title + " " + str(price) + " " + stock)
